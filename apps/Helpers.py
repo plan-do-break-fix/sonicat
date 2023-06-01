5164dbf5a2f5c93e53801003a22cf647d6157d86
JDSwan <plan.do.break.fix@gmail.com>
2023-06-01 12:26:22 -0500

    (apps) CheckPoint for resume on build fail

--- apps/Helpers.py
+++ apps/Helpers.py
@@ -1,5 +1,9 @@
 
+from contextlib import closing
 from dataclasses import dataclass
+import json
+import shutil
+from typing import Dict
 
 
 
@@ -12,3 +16,31 @@ class Config:
     log_level: str
     covers: str
     intake: str
+
+
+class CheckPoint:
+
+    def __init__(self, config: Config) -> None:
+        checkpoint_dir = f"{config.data}/checkpoints"
+        self.path = f"{config.data}/checkpoints/{config.app_name}"
+        self.major, self.minor = "", ""
+        if not shutil.os.path.exists(checkpoint_dir):
+            shutil.os.path.mkdir(checkpoint_dir)
+            return None
+
+    def read(self) -> Dict:
+        with closing(open(self.path ,"r")) as _f:
+            data = json.load(_f.read())
+        return data
+
+    def write(self) -> bool:
+        data = {"major": self.major, "minor": self.minor}
+        with closing(open(self.path ,"w")) as _f:
+            json.dump(data, _f)
+        return True
+
+    def set(self, major="", minor="") -> bool:
+        if not major:
+            raise ValueError("Major checkpoint must be set.")
+        self.major, self.minor = major, minor
+        self.write()

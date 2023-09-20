d8c7bc76bc2870f58143d23ee38a25aca669d711
JDSwan <plan.do.break.fix@gmail.com>
2023-09-20 11:54:23 -0500

    (refactor) changed call to Helpers

--- util/Logs.py
+++ util/Logs.py
@@ -1,6 +1,6 @@
 
 
-from apps.Helpers import Config
+from apps.ConfiguredApp import Config
 
 from datetime import datetime
 import logging
@@ -14,7 +14,7 @@ def initialize_logging(config: Config) -> logging.Logger:
     logger = logging.getLogger(config.app_name)
     logger.setLevel(LOOKUP[config.log_level])
     date_str = datetime.now().strftime('%Y-%m-%d')
-    log_path = f"{config.logging}/{date_str}-{config.app_name}.log"
+    log_path = f"{config.log}/{date_str}-{config.moniker}.log"
     fh = logging.FileHandler(log_path)
     FORMAT = "%(asctime)s | %(name)10s | %(levelname)5s | %(message)s"
     formatter = logging.Formatter(FORMAT)

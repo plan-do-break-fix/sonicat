a1b47d75d588733cf8affaed0b65834b8e830a15
JDSwan <plan.do.break.fix@gmail.com>
2023-09-29 15:27:35 -0500

    (util) updated name util

--- util/NameUtility.py
+++ util/NameUtility.py
@@ -2,18 +2,18 @@
 
 from typing import List, Tuple
 
-ENG_STOPWORDS = ["and", "in", "of", "the"]
-DOMAIN_STOPWORDS = [
-                    "audio",
-                    "loops",
-                    "music",
-                    "records",
-                    "samples",
-                    "sound",
-                    "sounds",
-                    "studio",
-                    "studios"
-                    ]
+#ENG_STOPWORDS = ["and", "in", "of", "the"]
+#DOMAIN_STOPWORDS = [
+#                    "audio",
+#                    "loops",
+#                    "music",
+#                    "records",
+#                    "samples",
+#                    "sound",
+#                    "sounds",
+#                    "studio",
+#                    "studios"
+#                    ]
 
 
 class NameUtility:
@@ -45,9 +45,17 @@ class NameUtility:
         label, title = parts[0], " - ".join(parts[1:])
         if " (" in title and title.endswith(")"):
             note = title.split(" (")[-1][:-1]
+            title = title.replace(f" ({note})", "")
         else:
             note = ""
         return (label, title, note)
+    
+    @staticmethod
+    def cname_tokens(cname: str) -> List[str]:
+        tokens, parts = [], NameUtility.divide_cname(cname)
+        for _part in parts:
+            tokens += _part.split(" ")
+        return tokens
 
     @staticmethod
     def label_dir_from_cname(cname: str) -> str:
@@ -86,45 +94,45 @@ class Variations(NameUtility):
             forms += self.join_tokens(self.tokenize(_v))
         return forms
     
-    def insert_optional_spaces(self, name: str) -> str:
-        allowed = [("drumkit", "drum kit"),
-                   ("hiphop", "hip hop"),
-                   ("lofi", "lo fi")
-                   ]
-        for _t in allowed:
-            if _t[0] in name:
-                name = name.replace(_t[0], _t[1])
-
-    def tokenize(self, name: str) -> List[str]:
-        """
-        Returns list of tokens from a publisher or product name.
-        """
-        if " - " in name:
-            name = name.replace(" - ", " ")
-        return name.lower().split(" ")
-       
-    def join_tokens(self, tokens: List[str], connectors=["", "-"]) -> List[str]:
-        """
-        Returns the string formed by joining <tokens> with a given connector, for each connector in connectors.
-        """
-        if len(tokens) == 1:
-            return tokens
-        output = []
-        for _c in connectors:
-            output.append(_c.join(tokens))
-        return output
-    
-    def drop_stopwords(self, tokens: List[str]):
-        """
-        Returns <tokens> without any members also found in a stopwords list.
-        """
-        to_drop = ENG_STOPWORDS + DOMAIN_STOPWORDS
-        return [_t for _t in tokens if _t not in to_drop]
-
-    def strip_stopwords(self, text: str):
-        """
-        Returns <text> with all stopwords removed.
-        """
-        tokens = text.split(" ")
-        tokens = self.drop_stopwords(tokens)
-        return " ".join(tokens)
+    #def insert_optional_spaces(self, name: str) -> str:
+    #    allowed = [("drumkit", "drum kit"),
+    #               ("hiphop", "hip hop"),
+    #               ("lofi", "lo fi")
+    #               ]
+    #    for _t in allowed:
+    #        if _t[0] in name:
+    #            name = name.replace(_t[0], _t[1])
+#
+    #def tokenize(self, name: str) -> List[str]:
+    #    """
+    #    Returns list of tokens from a publisher or product name.
+    #    """
+    #    if " - " in name:
+    #        name = name.replace(" - ", " ")
+    #    return name.lower().split(" ")
+    #   
+    #def join_tokens(self, tokens: List[str], connectors=["", "-"]) -> List[str]:
+    #    """
+    #    Returns the string formed by joining <tokens> with a given connector, for each connector in connectors.
+    #    """
+    #    if len(tokens) == 1:
+    #        return tokens
+    #    output = []
+    #    for _c in connectors:
+    #        output.append(_c.join(tokens))
+    #    return output
+    #
+    #def drop_stopwords(self, tokens: List[str]):
+    #    """
+    #    Returns <tokens> without any members also found in a stopwords list.
+    #    """
+    #    to_drop = ENG_STOPWORDS + DOMAIN_STOPWORDS
+    #    return [_t for _t in tokens if _t not in to_drop]
+#
+    #def strip_stopwords(self, text: str):
+    #    """
+    #    Returns <text> with all stopwords removed.
+    #    """
+    #    tokens = text.split(" ")
+    #    tokens = self.drop_stopwords(tokens)
+    #    return " ".join(tokens)

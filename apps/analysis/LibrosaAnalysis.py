4db7ee1d28edc564da39e7d600ce0d3c32271384
JDSwan <plan.do.break.fix@gmail.com>
2023-11-05 10:38:27 -0600

    (apps) Refactor generic analysis app to library specific apps

---
+++ apps/analysis/LibrosaAnalysis.py
@@ -0,0 +1,133 @@
+
+from decimal import *
+import librosa
+import numpy as np
+import shutil
+from typing import List, Tuple
+
+from apps.ConfiguredApp import App, Config
+from interfaces.LibrosaData import DataInterface
+from interfaces.Catalog import CatalogInterface
+from util import Logs
+from util.NameUtility import NameUtility
+
+
+DATATYPES = [
+    "tempo",
+    "duration"
+    "chroma_distribution",
+    "beat_frames"
+    ]
+
+class LibrosaAnalysis(App):
+
+    def __init__(self, sonicat_path: str) -> None:
+        super().__init__(sonicat_path, "")
+        self.basedir = f"{self.cfg.data}/analysis/"
+        self.data = DataInterface(f"{self.basedir}/LibrosaAnalysis.sqlite")
+        self.catalog = {
+            #"assets": CatalogInterface(f"{self.cfg.data}/catalog/AssetCatalog.sqlite"),
+            "releases": CatalogInterface(f"{self.cfg.data}/catalog/ReleaseCatalog-ReadReplica.sqlite")
+        } 
+        self.completed_assets = {_catalog: self.data.completed_assets(_catalog)
+                                 for _catalog in self.catalog_names}
+        self.file_type_id_cache = {}
+        self.cfg.log += "/analysis"
+        self.cfg.name = "AudioAnalysis"
+        self.log = Logs.initialize_logging(self.cfg)
+        self.log.info(f"LibrosaAnalysis Application Initialization Successful")
+
+    def get_file_type_id(self, catalog, ext) -> str:
+        if catalog not in self.file_type_id_cache.keys():
+            self.file_type_id_cache[catalog] = {}
+        if ext not in self.file_type_id_cache[catalog].keys():
+            _id = self.catalog[catalog].filetype_id(ext)
+            self.file_type_id_cache[catalog][ext] = _id
+        return self.file_type_id_cache[catalog][ext]
+
+    def load_wav(self, fpath: str) -> Tuple:
+        y, sr = librosa.load(fpath)
+        y_harmonic, y_percussive = librosa.effects.hpss(y)
+        return (y, sr, y_harmonic, y_percussive)
+    
+    def analyze_wav(self, path) -> Tuple:
+        y, sr, y_harmonic, y_percussive = self.load_wav(path)
+        duration = librosa.get_duration(y=y, sr=sr)
+        tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
+        duration, tempo = round(duration, 3), round(tempo, 1)
+        chromagram = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr)
+        for _i, _ in enumerate(chromagram):
+            for _j, _ in enumerate(chromagram[_i]):
+                if chromagram[_i][_j] < 1.0:
+                    chromagram[_i][_j] = 0
+        return (duration, tempo, beat_frames, chromagram)
+
+    def analyze_asset_wavs(self, catalog, asset_id) -> bool:
+        self.log.debug(f"Checking {catalog} asset ID {asset_id} for wav files.")
+        if asset_id in self.completed_assets:
+            self.log.debug(f"All wav files for asset ID {asset_id} previously analyzed.")
+            return True
+        asset_wav_file_ids = self.catalog[catalog].file_ids_by_asset_and_type(
+                               asset_id, self.get_file_type_id(catalog, "wav")
+                             )
+        self.log.debug(f"{len(asset_wav_file_ids)} wav file(s) to analyze in asset.")
+        self.catalog[catalog].export_asset_to_temp(asset_id,
+                                                   self.config["catalogs"][catalog]["path"]["managed"],
+                                                   f"{self.cfg.temp}-{catalog}"
+                                                   )
+        cname = self.catalog[catalog].asset_cname(asset_id)
+        label_dir = NameUtility.label_dir_from_cname(cname)
+        dfile_base_dir = f"{self.basedir}features/{catalog}/{label_dir}/{cname}"
+        if not shutil.os.path.isdir(dfile_base_dir):
+            shutil.os.makedirs(dfile_base_dir, exist_ok=True)
+        for _id in asset_wav_file_ids:
+            self.log.debug(f"Analyzing file ID {_id}.")
+            wav_path = f"{self.cfg.temp}-{catalog}/{cname}{self.catalog[catalog].file_path(_id)}"
+            duration, tempo, beat_frames, chromagram = self.analyze_wav(wav_path)
+            cdist = self.chromagram_to_cdist(chromagram)
+            self.log.debug("Wav file analysis complete. Writing data to disk.")
+            self.write_librosa_data(catalog, _id, duration, tempo)
+            self.write_beat_frames_dfile(catalog, _id, beat_frames, dfile_base_dir)
+            self.write_chroma_distribution(catalog, _id, cdist)
+        self.data.log_completed_asset(catalog, asset_id)
+        self.completed_assets[catalog].append(asset_id)
+        shutil.rmtree(f"{self.cfg.temp}-{catalog}/{cname}")
+        self.log.debug(f"Analysis of asset ID {asset_id} wav files complete.")
+
+
+    def chromagram_to_cdist(self, chromagram: np.ndarray) -> np.ndarray:
+        channel_sums = [sum(_i) for _i in chromagram]
+        total = sum(channel_sums)
+        return [str(Decimal(_sum/total))[:11] for _sum in channel_sums]
+
+    def write_librosa_data(self, catalog, file_id, duration, tempo) -> bool:
+        self.data.new_data(file_id, catalog, "1", dvalue=duration, finalize=False)
+        self.data.new_data(file_id, catalog, "2", dvalue=tempo, finalize=False)
+        self.data.db.commit()
+        return True
+    
+    def write_beat_frames_dfile(self, catalog, file_id, beat_frames, dfile_base_dir):
+        full_datapath = f"{dfile_base_dir}/{file_id}-librosa-beat_frames.npy"
+        np.save(full_datapath, beat_frames)
+        datapath = full_datapath.replace(self.cfg.data, "data")
+        self.data.new_data(file_id, catalog, "4", dpath=datapath, finalize=False)
+        return True
+
+    def write_chroma_distribution(self, catalog, file_id, chroma_distribution) -> str:
+        self.data.new_chroma_distribution(catalog, file_id, chroma_distribution)
+        self.data.c.execute("SELECT last_insert_rowid();")
+        cdist_id = self.data.c.fetchone()[0]
+        self.data.new_data(file_id, catalog, "3", dkeyid=cdist_id, finalize=False)
+        return True
+
+    def run(self, catalog):
+        all_ids = self.catalog[catalog].all_asset_ids()
+        assets_to_process = [_id for _id in all_ids if _id not in self.completed_assets[catalog]]
+        for _id in assets_to_process:
+            self.analyze_asset_wavs(catalog, _id)
+        return True
+    
+    def concurrent_run(self, catalog, i_assetid, f_assetid):
+        _ids = self.catalog[catalog].all_asset_ids_in_range(i_assetid, f_assetid)
+        assets_to_process = [_id for _id in all_ids if _id not in self.completed_assets[catalog]]
+
---
+++ interfaces/LibrosaData.py
@@ -0,0 +1,181 @@
+
+from typing import Dict, List, Tuple
+
+from interfaces.Interface import DatabaseInterface
+
+SCHEMA = [
+"""
+CREATE TABLE IF NOT EXISTS audiodata(
+  id integer PRIMARY KEY,
+  file integer NOT NULL,
+  catalog text NOT NULL,
+  datatype integer NOT NULL,
+  datavalue text,
+  datafilepath text,
+  dataforeignkey integer,
+  FOREIGN KEY (datatype)
+    REFERENCES audiodatatype (id)
+);
+""",
+"""
+CREATE TABLE IF NOT EXISTS audiodatatype (
+  id integer PRIMARY KEY,
+  name text NOT NULL
+);
+""",
+"""
+CREATE TABLE IF NOT EXISTS chromadistribution (
+  id integer PRIMARY KEY,
+  catalog text NOT NULL,
+  file integer NOT NULL,
+  c01 real NOT NULL,
+  c02 real NOT NULL,
+  c03 real NOT NULL,
+  c04 real NOT NULL,
+  c05 real NOT NULL,
+  c06 real NOT NULL,
+  c07 real NOT NULL,
+  c08 real NOT NULL,
+  c09 real NOT NULL,
+  c10 real NOT NULL,
+  c11 real NOT NULL,
+  c12 real NOT NULL
+);
+""",
+"""
+CREATE TABLE IF NOT EXISTS log (
+    id integer PRIMARY KEY,
+    asset integer NOT NULL,
+    catalog text NOT NULL
+);
+""",
+"""
+INSERT OR REPLACE INTO audiodatatype
+  (id, name)
+VALUES
+  (1, 'duration'),
+  (2, 'tempo'),
+  (3, 'chroma_distribution'), 
+  (4, 'beat_frames')
+;
+"""
+]
+
+
+class DataInterface(DatabaseInterface):
+
+    def __init__(self, dbpath=""):
+        super().__init__(dbpath)
+        for statement in SCHEMA:
+            self.c.execute(statement)
+        self.db.commit()
+        self.dtype_ids = {}
+
+    def new_data(self, file_id: str,
+                       catalog: str,
+                       dtype_id: str,
+                       dvalue="",
+                       dpath="",
+                       dkeyid="",
+                       finalize=False
+                       ) -> bool:
+        if dvalue and not (dpath or dkeyid):
+            self.c.execute("""
+            INSERT INTO audiodata (file, catalog, datatype, datavalue)
+            VALUES (?,?,?,?);
+            """, (file_id, catalog, dtype_id, dvalue))
+        elif dpath and not (dvalue or dkeyid):
+            self.c.execute("""
+            INSERT INTO audiodata (file, catalog, datatype, datafilepath)
+            VALUES (?,?,?,?);
+            """, (file_id, catalog, dtype_id, dpath))
+        elif dkeyid and not (dvalue or dpath):
+            self.c.execute("""
+            INSERT INTO audiodata (file, catalog, datatype, dataforeignkey)
+            VALUES (?,?,?,?);
+            """, (file_id, catalog, dtype_id, dkeyid))
+        else:
+            return True
+        if finalize:
+            self.db.commit()
+        return True
+
+    def new_chroma_distribution(self, catalog, file_id, cdist):
+        if not len(cdist) == 12:
+            raise ValueError
+        self.c.execute("""
+        INSERT INTO chromadistribution
+          (catalog, file, c01, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12)
+        VALUES
+          (?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+        ;""", (catalog, file_id, cdist[0], cdist[1], cdist[2], cdist[3],
+                                 cdist[4], cdist[5], cdist[6], cdist[7],
+                                 cdist[8], cdist[9], cdist[10], cdist[11]))
+
+    def dtype_id(self, dtype_name: str) -> str:
+        if not dtype_name in self.dtype_ids.keys():
+            self.c.execute("SELECT id FROM audiodatatype WHERE name = ?",
+                           (dtype_name,))
+            self.dtype_ids[dtype_name] = self.c.fetchone()[0]
+        return self.dtype_ids[dtype_name]
+    
+    def dtype(self, dtype_id: str) -> str:
+        self.c.execute("SELECT name FROM audiodatatype WHERE id = ?",
+                       (dtype_id,))
+        return self.c.fetchone()[0]
+    
+    def data_value(self, file_id, catalog, dtype_id) -> Tuple[str]:
+        self.c.execute("""
+        SELECT id, datavalue FROM audiodata
+        WHERE file = ? AND catalog = ? AND datatype = ?
+        ;""", (file_id, catalog, dtype_id))
+        return self.c.fetchone()
+    
+    def data_file_path(self, file_id, catalog, dtype_id) -> Tuple[str]:
+        self.c.execute("""
+        SELECT id, datafilepath FROM audiodata
+        WHERE file = ? AND catalog = ? AND datatype = ?
+        ;""", (file_id, catalog, dtype_id))
+        return self.c.fetchone()
+    
+    def data_foreign_key(self, file_id, catalog, dtype_id) -> Tuple[str]:
+        self.c.execute("""
+        SELECT id, dataforeignkey FROM audiodata
+        WHERE file = ? AND catalog = ? AND datatype = ?
+        ;""", (file_id, catalog, dtype_id))
+        return self.c.fetchone()
+    
+    def all_files_having_data(self, catalog, dtype_id) -> List[str]:
+        self.c.execute("""
+        SELECT file FROM audiodata
+        WHERE catalog = ? AND datatype = ?
+        ORDER BY file ASC
+        ;""", (catalog, dtype_id))
+        return [_i[0] for _i in self.c.fetchall()]
+    
+    def log_completed_asset(self, catalog, asset_id):
+        self.c.execute("INSERT INTO log (catalog, asset) VALUES (?,?);",
+                       (catalog, asset_id))
+        self.db.commit()
+
+    def completed_assets(self, catalog) -> List[str]:
+        self.c.execute("SELECT asset FROM log WHERE catalog = ?;", (catalog,))
+        return [_i[0] for _i in self.c.fetchall()]
+    
+    def all_chroma_distributions(self, catalog, lbound=0, ubound=0) -> Dict[str, List[float]]:
+        query = "SELECT * FROM chromadistribution WHERE catalog = ?"
+        arguments = [catalog]
+        if lbound > 0:
+            query += " AND file > ?"
+            arguments.append(lbound)
+        if ubound > 0:
+            query += " AND file < ?"
+            arguments.append(ubound)
+        query += " ORDER BY file ASC;"
+        self.c.execute(query, arguments)
+        return {_i[2]: _i[3:] for _i in self.c.fetchall()}
+
+
+
+        
+        

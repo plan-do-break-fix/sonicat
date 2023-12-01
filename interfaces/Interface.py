2e498e881bfc0e78391af00b5d6c007bc7c382cf
JDSwan <plan.do.break.fix@gmail.com>
2023-12-01 16:05:10 -0600

    WOT-190 (interfaces) updated WebInterface

--- interfaces/Interface.py
+++ interfaces/Interface.py
@@ -27,25 +27,49 @@ class DatabaseInterface:
         self.c = self.db.cursor()
         
 
-import requests
 
-from bs4 import BeautifulSoup
+import requests
+import time
+
 from user_agent import generate_user_agent
+
+from typing import Dict
+
+THROTTLE_TIME = 4
+USER_AGENT_TTL = 300
+IP_ADDR_TTL = 300
 
 
 class WebInterface:
 
     def __init__(self):
         self.useragent = generate_user_agent()
+        self.wait = THROTTLE_TIME
+        self.t = {_k: time.time() for _k in ["throttle", "useragent", "ip"]}
 
     def html_encode(self, query: str) -> str:
         query = query.replace(" - ", " ")
         return "%20".join(query.split(" "))
     
-    def headers(self, useragent: str) -> str:
+    def headers(self, useragent: str) -> Dict:
         return {"User-Agent": useragent}
 
+    def cycle_useragent(self) -> bool:
+        self.useragent = generate_user_agent()
+        self.t["useragent"] = time.time()
+        return True
+
+    def cycle_ip(self) -> None:
+        pass
+
+    def throttle(self):
+        if time.time() - self.t["throttle"] < self.wait:
+            time.sleep(self.wait - (time.time() - self.t["throttle"]))
+        self.t["throttle"] = time.time()
+        return True
+
     def get_content(self, url: str) -> str:
+        self.throttle()
         resp = requests.get(url, headers=self.headers(self.useragent))
         try:
             if resp.status_code == 200:

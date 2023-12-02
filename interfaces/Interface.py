7864d131f1ad91b71435f4aea9a925be9d999d56
JDSwan <plan.do.break.fix@gmail.com>
2023-12-01 22:17:30 -0600

    WOT-240 (interfaces) web driver support

--- interfaces/Interface.py
+++ interfaces/Interface.py
@@ -29,11 +29,13 @@ class DatabaseInterface:
 
 
 import requests
+from selenium import webdriver
 import time
 
 from user_agent import generate_user_agent
-
-from typing import Dict
+# for typing
+from typing import Dict, Union
+from requests import Response
 
 THROTTLE_TIME = 4
 USER_AGENT_TTL = 300
@@ -44,15 +46,26 @@ class WebInterface:
 
     def __init__(self):
         self.useragent = generate_user_agent()
+        self.login_url, self.session = False, False
         self.wait = THROTTLE_TIME
         self.t = {_k: time.time() for _k in ["throttle", "useragent", "ip"]}
+
+    def login(self, username, password):
+        if not self.login_url:
+            print("WebInterface subclass must implement login_url attr to log in.")
+            raise RuntimeError
+        if not self.session:
+            self.session = requests.Session()
+        payload = {"username": username, "password": password}
+        resp = self.post(self.login_url, payload)
+        return bool(resp)
 
     def html_encode(self, query: str) -> str:
         query = query.replace(" - ", " ")
         return "%20".join(query.split(" "))
     
-    def headers(self, useragent: str) -> Dict:
-        return {"User-Agent": useragent}
+    def headers(self) -> Dict:
+        return {"User-Agent": self.useragent}
 
     def cycle_useragent(self) -> bool:
         self.useragent = generate_user_agent()
@@ -68,20 +81,18 @@ class WebInterface:
         self.t["throttle"] = time.time()
         return True
 
-    def get_content(self, url: str) -> str:
+    def get_html_with_driver(url):
+        driver = webdriver.Chrome()
+        driver.get(url)
+        source = driver.page_source
+        driver.quit()
+        return source
+
+    def get(self, url: str, headers={}) -> requests.Response:
+        headers=self.headers() if not headers else headers
         self.throttle()
-        resp = requests.get(url, headers=self.headers(self.useragent))
         try:
-            if resp.status_code == 200:
-                return resp.content
-            elif 300 <= resp.status_code < 400:
-                pass
-            elif 400 <= resp.status_code < 500:
-                pass
-            elif 500 <= resp.status_code < 600:
-                pass
-            else:
-                raise requests.HTTPError
+            resp = requests.get(url, headers=self.headers(self.useragent))
         except ConnectionError:
             return e
         except requests.HTTPError as e:
@@ -90,10 +101,37 @@ class WebInterface:
             return e
         except TimeoutError:
             return e
-        finally:
-            print("How did you get here?")
-            raise RuntimeError
+        return resp if resp.ok else self.handle_response(resp)
 
+    def post(self, url: str, payload: Dict):
+        if not self.session:
+            return False
+        headers=self.headers() if not headers else headers
+        self.throttle()
+        try:
+            resp = self.session.post(url, data=payload, headers=headers)
+        except ConnectionError:
+            return e
+        except requests.HTTPError as e:
+            return e
+        except requests.TooManyRedirects:
+            return e
+        except TimeoutError:
+            return e
+        return resp if resp.ok else self.handle_response(resp)
+
+    def handle_response(self, resp: Response) -> Union[Response, bool]:
+        if resp.status_code == 200:
+            return resp
+        elif 300 <= resp.status_code < 400:
+            pass
+        elif 400 <= resp.status_code < 500:
+            pass
+        elif 500 <= resp.status_code < 600:
+            pass
+        else:
+            raise requests.HTTPError
+        return False
 
     def retry(self, url):
         pass

0ee8c0f79677254138b3802993952e14526f49c1
JDSwan <plan.do.break.fix@gmail.com>
2023-12-01 22:16:49 -0600

    WOT-238 (apps) credentials for scraper apps

--- apps/metadata/webscraper/ScraperApp.py
+++ apps/metadata/webscraper/ScraperApp.py
@@ -1,7 +1,10 @@
 
+from contextlib import closing
+from yaml import load, SafeLoader
 
 from apps.ConfiguredApp import SimpleApp
-from interfaces.database.Catalog import CatalogInterface
+
+from typing import Tuple
 
 
 class ScraperApp(SimpleApp):
@@ -10,5 +13,10 @@ class ScraperApp(SimpleApp):
         super().__init__(sonicat_path, "metadata", app_name)
         self.load_catalog_replicas()
     
+    def get_credentials(self, secrets_key: str) -> Tuple[str]:
+        with closing(open(f"{self.cfg['sonicat_path']}/config/secrets.yaml", "r")) as _f:
+            secret = load(_f.read(), SafeLoader)
+        return (secret[secrets_key]["uname"], secret[secrets_key]["passwd"])
+
     def run(self):
         pass
